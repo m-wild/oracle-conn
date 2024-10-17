925356d0efd3a6d11e21b81acd2a47bea25e0110
Michael Wildman <michael.wildman@2degrees.nz>
2024-10-17 20:28:35 +1300

    simple oracle connection tester

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.CommandLine;
+using Oracle.ManagedDataAccess.Client;
+
+var cs = new Option<string>("--connection-string", "Oracle DB connection string");
+var cmd = new RootCommand("Test oracle DB connectivity");
+cmd.AddOption(cs);
+
+cmd.SetHandler(async (context) => {
+
+    var connectionString = context.ParseResult.GetValueForOption(cs);
+
+    using var conn = new OracleConnection(connectionString);
+    await conn.OpenAsync(context.GetCancellationToken());
+
+    
+    Console.WriteLine($"ConnectionID     = {conn.ConnectionId}");
+    Console.WriteLine($"State            = {conn.State}");
+    Console.WriteLine($"ConnectionString = {conn.ConnectionString}");
+    Console.WriteLine($"DatabaseName     = {conn.DatabaseName}");
+    Console.WriteLine($"HostName         = {conn.HostName}");
+    Console.WriteLine($"InstanceName     = {conn.InstanceName}");
+    Console.WriteLine($"ServerVersion    = {conn.ServerVersion}");
+    Console.WriteLine($"ServiceName      = {conn.ServiceName}");
+});
+
+await cmd.InvokeAsync(args);
